67e21ecb2e9779608ba4b7fcb1167993e1ee428c
rafael <rafaelsm003@gmail.com>
2022-04-26 18:34:23 -0300

    [FEAT]: requisito 5 adicionado

--- src/services/api.js
+++ src/services/api.js
@@ -21,3 +21,14 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
     console.log(error);
   }
 }
+
+export async function getItemsByQuery(query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  try {
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
