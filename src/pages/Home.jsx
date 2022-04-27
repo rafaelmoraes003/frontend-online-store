7d7b53557f013c2679416a60ee53876e3fce594d
Felipe Avila <f.avila@hotmail.com.br>
2022-04-27 14:54:29 -0300

    Feita estilização basica do projeto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { getCategories, getItemsByQuery } from '../services/api';
 
+import './Home.style.css';
+
 class Home extends React.Component {
   constructor() {
     super();
@@ -28,19 +30,6 @@ class Home extends React.Component {
     const { search, categories, cards } = this.state;
     return (
       <div className="container">
-        <aside>
-          <div className="categories">
-            { categories.map((item) => (
-              <button
-                type="button"
-                key={ item.id }
-                data-testid="category"
-              >
-                { item.name }
-              </button>
-            ))}
-          </div>
-        </aside>
         <div className="search-area">
           <input
             type="text"
@@ -48,12 +37,6 @@ class Home extends React.Component {
             data-testid="query-input"
             onChange={ (event) => this.setState({ search: event.target.value }) }
           />
-          <Link
-            to="/cart"
-            data-testid="shopping-cart-button"
-          >
-            <i className="fa-solid fa-cart-shopping" />
-          </Link>
           <button
             onClick={ this.fetchByQuery }
             data-testid="query-button"
@@ -61,22 +44,53 @@ class Home extends React.Component {
           >
             Pesquisar
           </button>
-          <p data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
+          <Link
+            to="/cart"
+            data-testid="shopping-cart-button"
+          >
+            <i className="fa-solid fa-cart-shopping" />
+          </Link>
         </div>
-        {cards.length < 1 && <p>Nenhum produto foi encontrado</p>}
-        {cards.length > 1 && (
-          <div className="card">
-            {cards.map((card) => (
-              <div key={ card.id } data-testid="product">
-                <img src={ card.thumbnail } alt={ card.title } />
-                <h3>{ card.title }</h3>
-                <h4>{ card.price }</h4>
-              </div>
-            ))}
-          </div>
-        )}
+        <div className="content-area">
+          <aside>
+            <div className="categories">
+              { categories.map((item) => (
+                <div className="categorie" key={ item.id }>
+                  <button
+                    type="button"
+                    key={ item.id }
+                    data-testid="category"
+                  >
+                    { item.name }
+                  </button>
+                </div>
+              ))}
+            </div>
+          </aside>
+          <section>
+            <div className="cards-area">
+              <p data-testid="home-initial-message">
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </p>
+              {cards.length < 1 && <p>Nenhum produto foi encontrado</p>}
+              {cards.length > 1 && (
+                <div className="cards">
+                  {cards.map((card) => (
+                    <div className="card" key={ card.id } data-testid="product">
+                      <img src={ card.thumbnail } alt={ card.title } />
+                      <h3>{ card.title }</h3>
+                      <h4>
+                        R$
+                        { card.price }
+                      </h4>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </section>
+        </div>
+
       </div>
     );
   }
